1b5c317d12cc7f6df701fe2a22f43a3e769353f9
Patrick-Gabriel-ACC <patrick.gabriel@g.austincc.edu>
2025-03-30 04:45:22 +0000

    HW4

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,41 @@
-#
+import strings
+
+def main():
+    
+    while True:
+        print("Menu")
+        print("1 - Hamming Distance")
+        print("2 - DNA Complement")
+        print("3 - Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            dna1 = input("Enter first DNA string: ").upper()
+            dna2 = input("Enter second DNA string: ").upper()
+            
+            try:
+                distance = get_hamming_distance(dna1, dna2)
+                print("Hamming Distance:", distance)
+            
+            except ValueError as e:
+                print("Error:", e)
+        
+        elif choice == '2':
+            dna = input("Enter DNA string: ").upper()
+            
+            try:
+                complement = get_dna_complement(dna)
+                print("DNA Complement:", complement)
+            except ValueError as e:
+                print("Error:", e)
+        
+        elif choice == '3':
+            print("Goodbye!")
+            
+            break
+        
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+
+main()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,51 @@
-#
+def get_hamming_distance(dna1, dna2):
+
+    if len(dna1) != len(dna2):
+
+        raise ValueError("DNA sequences must be of equal length.")
+    
+    distance = 0
+
+    index = 0
+
+    while index < len(dna1):
+
+        if dna1[index] != dna2[index]:
+
+            distance += 1
+
+        index += 1
+    
+    return distance
+
+    print(distance)
+
+
+def get_dna_complement(dna):
+    complement = ''
+    
+    reversed_dna = dna[::-1]
+    
+    
+    for base in reversed_dna:
+        
+        if base == 'A':
+            complement += 'T'
+        
+        elif base == 'T':
+            complement += 'A'
+        
+        elif base == 'C':
+            complement += 'G'
+        
+        elif base == 'G':
+            complement += 'C'
+        
+        else:
+            
+            raise ValueError("Invalid DNA base: " + base)
+    
+    return complement
+
+
+    print(complement)
