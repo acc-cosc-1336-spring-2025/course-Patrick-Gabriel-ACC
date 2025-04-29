6672c4bb6f9ad0ed834e5be3794ee17758e6cf8b
Patrick-Gabriel-ACC <patrick.gabriel@g.austincc.edu>
2025-04-29 03:37:56 +0000

    HW8

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,16 @@
 #
+
+import random
+
+class die:
+    
+    def roll(self):
+        self.__roll_value = random.randint(1, 6)
+
+    def get_rolled_value(self):
+        
+        return self.__roll_value
+
+    def __str__(self):
+        
+        return f"The rolled value is {self.__roll_value}"
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,18 @@
 #
+
+import unittest
+
+from src.homework.j_classes.class_a import die
+
+class TestDie(unittest.TestCase):
+    
+    def test_die_roll(self):
+        d = die()
+       
+        for _ in range(3):
+            d.roll()
+            val = d.get_rolled_value()
+            self.assertIn(val, range(1, 7), "Roll value out of bounds!")
+
+if __name__ == "__main__":
+    unittest.main()
